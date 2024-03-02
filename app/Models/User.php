a1402c3b156e8a8ba724030a83edec9d7581d8af
Ian <ramas.ian2003@gmail.com>
2024-03-02 14:21:15 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+ namespace App\Models;
+ use Illuminate\Database\Eloquent\Model;
+ class User extends Model{
+    protected $table = 'students';
+    // column sa table
+    protected $fillable = [
+    'username', 'password'
+    ];
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+ }
